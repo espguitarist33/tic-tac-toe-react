d9085ae9968b44e72615e91e8878ce2186f4619f
espguitarist33 <ryan.pisano@vertexinc.com>
2021-12-26 13:47:41 -0500

    Lots of corrections... things got weird

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-const react = require("react");
-
 import React from 'react';
-import reactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends react.Component {
+class Square extends React.Component {
     render() {
         return (
             <button className="square">
