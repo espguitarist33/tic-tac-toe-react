8d186661a8138daab7f0e15a5754747975d2d2d6
espguitarist33 <ryan.pisano@vertexinc.com>
2021-12-26 20:00:44 -0500

    Pulled up game state from Board to Game to prepare for history

--- src/index.js
+++ src/index.js
@@ -16,52 +16,21 @@ function Square(props) {
     }
 
 class Board extends React.Component {
-    constructor(props) {
-        // Let's start with an empty state for the whole board
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        // This re-renders all of them, wonder if that's even necessary at this point?
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-    
     renderSquare(i) {
         //Now we will tell the square it's state from our array. React calls the Squares as "controlled components"
         // handleClick allows the state to be updated via call-back and keeps the components correctly encapsulated (like pub/sub)
         return (
             <Square
-                value={this.state.squares[i]}
+                value={this.props.squares[i]}
                 // Note that this property name doesn't actually matter.. <button>'s implementation does, however.
-                onClick={() => this.handleClick(i)}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -83,15 +52,54 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        }
+    }
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        // This re-renders all of them, wonder if that's even necessary at this point?
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            xIsNext: !this.state.xIsNext,
+        });        
+        
+    }
     render () {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}/>
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <div>{/* TODO */}</div>
+                    <div>{status}</div>
+                    <ol>{/* TODO */}</ol>
                 </div>
             </div>
         );
@@ -116,7 +124,7 @@ function calculateWinner(squares) {
     ];
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
-        if (squares [a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a];
         }
     }
